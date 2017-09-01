ba0df8831677da5835e8f6b8afa50514487ad93c
Balint Fulop <bfulop@mac.com>
2017-09-01 11:06:49 +0200

    Added Mocha and Chai

---
+++ src/a.test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* global expect, describe, it */
+var expect = require('chai').expect
+
+describe('first test', function () {
+  it('should work', function () {
+    return expect(true).to.be.true
+  })
+})
