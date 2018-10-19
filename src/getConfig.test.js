c6a068be4b965929d619909a0e9ecf18b923f0c9
Balint Fulop <bfulop@mac.com>
2018-10-19 20:11:29 +0200

    Using Jest test runner, getConfig passing

--- src/getConfig.test.js
+++ src/getConfig.test.js
@@ -1,21 +1,13 @@
-'use strict'
-
 var Task = require('data.task')
+jest.mock('./utils/fileUtils')
+var fileUtils = require('./utils/fileUtils')
 
-var fileUtils = td.replace('./utils/fileUtils')
-td
-  .when(fileUtils.readFile('../config.json'))
-  .thenReturn(
-    Task.of(JSON.stringify({ mongodb: 'foo' }))
-  )
-
-var subject = require('./getConfig')
-
-context('first run', function () {
-  it('should be ok', function (done) {
-    subject.getConfig.fork(console.error, t => {
-      expect(t).to.eql({ mongodb: 'foo' })
-      done()
-    })
+test('should get the config', done => {
+  const configfile = Task.of(JSON.stringify({ mongodb: 'foo' }))
+  fileUtils.readFile.mockReturnValue(configfile)
+  var subject = require('./getConfig')
+  subject.getConfig.fork(console.error, t => {
+    expect(t).toEqual({ mongodb: 'foo' })
+    done()
   })
 })
--- src/getconfig.js
+++ src/getconfig.js
@@ -2,12 +2,8 @@ const fileUtils = require('./utils/fileUtils')
 const { tryCatch } = require('./utils/either')
 const Task = require('data.task')
 
-const getConfig = fileUtils
-  .readFile('./config.json')
-  .map(config => tryCatch(() => JSON.parse(config)))
-  .chain(r => r.fold(
-    Task.rejected,
-    c => Task.of(c)
-    ))
+const getConfig = fileUtils.readFile('./config.json')
+.map(config => tryCatch(() => JSON.parse(config)))
+.chain(r => r.fold(Task.rejected, c => Task.of(c)))
 
-module.exports = {getConfig}
+module.exports = { getConfig }
