52f5b871708fba98f09634c4ef89e451efbc8b83
Balint Fulop <bfulop@mac.com>
2018-10-20 10:05:25 +0200

    make fake more explicit

--- src/getConfig.test.js
+++ src/getConfig.test.js
@@ -3,11 +3,11 @@ jest.mock('./utils/fileUtils')
 var fileUtils = require('./utils/fileUtils')
 
 test('should get the config', done => {
-  const configfile = Task.of(JSON.stringify({ mongodb: 'foo' }))
+  const configfile = Task.of(JSON.stringify({ foo: 'bar' }))
   fileUtils.readFile.mockReturnValue(configfile)
   var subject = require('./getConfig')
   subject.getConfig.fork(console.error, t => {
-    expect(t).toEqual({ mongodb: 'foo' })
+    expect(t).toEqual({ foo: 'bar' })
     done()
   })
 })
