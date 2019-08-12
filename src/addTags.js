8b6d9a4572d826be157f5c40553275a25eee25f0
Balint Fulop <bfulop@mac.com>
2019-08-12 17:34:52 +0200

    Make auto tagging less greedy don't search notes content and titles

--- src/addTags.js
+++ src/addTags.js
@@ -15,37 +15,24 @@ const calcProp = (p, f) =>
     ),
     R.identity
   )
+
 const calcPropR = R.curry(calcProp)
-const sometext = {
-  data: 'pants blah3 shoes tag1 blah',
-  tags: []
-}
-const content = [
-  sometext,
-  {
-    data: 'relatedtg4 none nothing anything'
-  }
-]
-const config = {
-  dictionary: [
-    { name: 'tag1', related: ['blah', 'foo'] },
-    { name: 'tag2', related: ['blah2', 'foo2'] },
-    { name: 'tag3', related: ['blah3', 'foo3'] },
-    { name: 'tag4', related: ['relatedtg4'] },
-    { name: 'tag5', related: ['relatedtg5'] }
-  ]
-}
 
 const nameLens = R.lensProp('name')
+
 const intoArray = R.append(R.__, [])
-const converttoRegex = t => new RegExp('(\\s+|^)' + t + '(\\s+|$)', 'i', 'm')
+
+const converttoRegex = t => new RegExp('(\\s+|^)' + t + '(\\s+|$)', 'u')
+
 const mergenames = R.compose(
   R.map(converttoRegex),
   R.flatten,
   R.values,
   R.over(nameLens, intoArray)
 )
+
 const addmerged = calcPropR('merged', mergenames)
+
 const containsTxt = txt => R.test(R.__, txt)
 
 const mergeTagname = R.converge(
@@ -55,28 +42,37 @@ const mergeTagname = R.converge(
       R.identity
     ]
   )
+
 const containsWord = R.curry((word, dictitem) => R.contains(word, dictitem))
+
 const getDictionaryTag = R.curry((dict, word) => R.compose(
   R.map(R.prop('name')),
   R.filter(R.compose(R.contains(word), R.prop('related'))),
   R.map(mergeTagname)
 )(dict))
+
 const testlist = txt => R.any(containsTxt(txt))
+
 const testline = txt =>
   R.compose(
     testlist(txt),
     R.view(R.lensProp('merged')),
     addmerged
   )
+
 const getTags = txtblock =>
   R.compose(
     R.map(R.prop('name')),
     R.filter(testline(txtblock.data))
   )
+
 const getTagsC = R.curry((dict, txtblock) => getTags(txtblock)(dict))
+
 const addTags = dict =>
   R.converge(R.assoc('tags'), [getTagsC(dict), R.identity])
+
 const addAllTags = dict => R.map(addTags(dict))
+
 const getAllTags = dict =>
   R.compose(
     R.uniq,
@@ -86,33 +82,36 @@ const getAllTags = dict =>
   )
 
 const concatAll_ = R.unapply(R.reduce(R.concat, []))
+
 const getTagsEveryWhere = dict =>
   R.converge(concatAll_, [
     R.compose(
       R.flatten,
       R.map(getDictionaryTag(dict)),
-      R.view(R.lensPath(['note','meta', 'tags']))
+      R.view(R.lensPath(['note', 'meta', 'tags']))
     ),
-    R.compose(
-      getTagsC(dict),
-      R.objOf('data'),
-      R.path(['note','content', 'title'])
-    ),
-    R.compose(
-      getTagsC(dict),
-      R.objOf('data'),
-      R.path(['nbook', 'name'])
-    ),
-    R.compose(
-      getAllTags(dict),
-      R.path(['note','content', 'cells'])
-    )
+    // R.compose(
+    //   getTagsC(dict),
+    //   R.objOf('data'),
+    //   R.path(['note','content', 'title'])
+    // ),
+    // R.compose(
+    //   getTagsC(dict),
+    //   R.objOf('data'),
+    //   R.path(['nbook', 'name'])
+    // ),
+    // R.compose(
+    //   getAllTags(dict),
+    //   R.path(['note','content', 'cells'])
+    // )
   ])
-const collectTags = dict =>
-  R.converge(R.set(R.lensPath(['note','meta', 'tags'])), [
+
+function collectTags(dict) {
+  return R.converge(R.set(R.lensPath(['note','meta', 'tags'])), [
     R.compose(R.uniq, getTagsEveryWhere(dict)),
     R.identity
   ])
+} 
 
 const findTags = r => getConfig().map(c => collectTags(c.dictionary)(r))
 
